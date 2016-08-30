a073e8f1a623eb02ada0f3d6d877ce5d7b4c1780
zenderol <zenderoleg@gmail.com>
2016-08-31 00:11:58 +0200

    added severak grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
+    var mozjpeg = require('imagemin-mozjpeg');
 
     grunt.initConfig({
 
@@ -41,6 +42,50 @@ module.exports = function(grunt) {
                 src: ['**/*.{sass,scss}'],
                 dest: 'styleguide/'
             }
+        },
+        imagemin: {                          // Task
+            static: {                          // Target
+                options: {                       // Target options
+                    optimizationLevel: 4,
+                    svgoPlugins: [{ removeViewBox: false }],
+                    use: [mozjpeg()]
+                },
+                files: {                         // Dictionary of files
+                    'dist/img.png': 'src/img.png', // 'destination': 'source'
+                    'dist/img.jpg': 'src/img.jpg',
+                    'dist/img.gif': 'src/img.gif'
+                }
+            },
+            dynamic: {                         // Another target
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: 'src/',                   // Src matches are relative to this path
+                    src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'dist/'                  // Destination path prefix
+                }]
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'dist/index.js': ['src/index.js']
+                }
+            }
+        },
+        compress: {
+            main: {
+                options: {
+                    mode: 'gzip',
+                    level: 9
+                },
+                expand: true,
+                cwd: 'dist/',
+                src: ['**/*.js'],
+                dest: 'dist/compressed'
+            }
         }
 
     });
@@ -50,8 +95,12 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
     // Register Grunt tasks
     grunt.registerTask('default', ['clean:pre', 'compass', 'sassdown', 'clean:post']);
+    grunt.registerTask('compess_images', ['imagemin:dynamic']);
   
 };
